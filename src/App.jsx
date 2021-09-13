660d4d09f06b18af54775441c57a56ccb1a4c783
FlorencaAnes <mflorenciaanes@gmail.com>
2021-09-13 22:04:03 +0200

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from "./components/Nav"
+
+
+
+const App = () => {
+    return (
+
+        <header>
+        <p className="brand-tittle">Cocolin Press</p>
+        <Nav/>
+        </header>
+    )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import "./nav.css"
+
+const Nav = () => {
+    return (
+        <nav className="menu">        
+             <a href="#">Characters</a>
+             <a href="#">Fanzines</a>
+             <a href="#">Shop</a>
+             <a href="#">About</a>
+        </nav>
+    )
+}
+
+export default Nav;
